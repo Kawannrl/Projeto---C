c3f63b68344046958ff6ef9b35cc342e5eaca374
Kawannrl <165685845+Kawannrl@users.noreply.github.com>
2024-09-19 16:28:11 -0300

    Update Atividade17.c

--- Atividade17.c
+++ Atividade17.c
@@ -1,103 +1,119 @@
-# include <stdio.h>
-# include <string.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int n = 0;
-
-struct st_contatos
-{
-    char nome [100], telefone [100], email [100], endereco [100];
+struct st_aluno{
+    char nome[100], telefone[100], endereco[100], email[100];
+    int n;
 };
 
-struct st_agenda
-{
-    struct st_contatos contato [100];
-} agenda;
+struct st_agenda{
+    struct st_aluno alunos;
+}agenda[100];
 
-void chama_menu (){
+void inserir(int* n){
 
-    int opcao;
+    printf("Digite quantas pessoas pretende inserir: ");
+    scanf("%d", &*n);
+    getchar();
 
-    do {
-        printf ("\nEscolha uma opcao: ");
-        printf ("\n1 - Inserir\n2 - Excluir\n3 - Buscar\n4 - Listar contatos\n5 - Sair\nOpcao: ");
-        scanf ("%d", &opcao);
-        system ("cls");
-    } while (opcao != 1 && opcao != 2 && opcao != 3 && opcao != 4 && opcao != 5);
-
-    switch (opcao){
-
-        case 1:
-        inserir ();
-        chama_menu();
-        break;
-        case 2:
-        excluir ();
-        chama_menu ();
-        break;
-        case 3:
-        buscar ();
-        chama_menu ();
-        break;
-        case 4:
-        listar_contatos ();
-        chama_menu ();
-        break;
-        case 5:
-        printf ("\nPrograma encerrado pelo usuario");
-        break;
-        default:
-        printf ("Opcao Invalida\n\nInforme uma opcao valida");
-        chama_menu ();
-        break;
+ for (int i = 0; i < *n; i++){
+        printf("Digite o nome do %d aluno: ", i+1);
+        fgets(agenda[i].alunos.nome, 100, stdin);
+        printf("Digite o Telefone: ");
+        fgets(agenda[i].alunos.telefone, 100 ,stdin);
+        printf("Digite o email: ");
+        fgets(agenda[i].alunos.email, 100, stdin);
+        printf("Digite o endereco: ");
+        fgets(agenda[i].alunos.endereco, 100, stdin);
+        system("cls");
     }
 }
 
-void inserir (){
+void listar_contatos(int *n){
     
-    if (agenda.contato > 100){
-        printf ("Informe quantos numeros deseja inserir: ");
-        scanf ("%d", &n);
 
-        for (int i = 0; i < n; i++){
-            printf ("Informe seu nome: ");
-            fgets (agenda.contato [i].nome, 100, stdin);
-            getchar ();
-            printf ("Informe seu telefone: ");
-            fgets (agenda.contato [i].telefone, 100, stdin);
-            getchar ();
-            printf ("Informe seu email: ");
-            fgets (agenda.contato [i].email, 100, stdin);
-            getchar ();
-            printf ("Informe seu endereco: ");
-            fgets (agenda.contato [i].endereco, 100, stdin);
-            getchar ();
+    //printf("Digite quantas opcoes gostaria de listar: ");
+    //scanf("%d", &*n);
+
+    for (int i = 0; i < *n; i++){
+        printf("Nome: %sTelefone: %sE-mail: %sEndereco: %s\n", agenda[i].alunos.nome, agenda[i].alunos.telefone, agenda[i].alunos.email, agenda[i].alunos.endereco);
+    }
+}
+
+void buscar(int *n){
+
+    int pessoa;
+
+    printf("Tem um total de %d casos", *n);
+    printf("\nDigite qual pessoa voce gostaria de avaliar: ");
+    scanf("%d", &pessoa);
+
+    for (int i = 0; i < *n; i++){
+        if (pessoa == i+1){
+            printf("Nome: %sTelefone: %sE-mail: %sEndereco: %s\n", agenda[i].alunos.nome, agenda[i].alunos.telefone, agenda[i].alunos.email, agenda[i].alunos.endereco);
         }
-    } else {
-        printf ("Agenda cheia!!!\nExclua um numero para poder registrar um novo numero!");
+        else{
+            printf("Contato nao encontrado");
+        }
     }
 }
 
-void excluir (){
+void excluir (int *n){
+       int pessoa, resp;
+       
 
-    
-    
-}
+    printf("Tem um total de %d casos", *n);
+    printf("\nDigite qual pessoa voce gostaria de excluir: ");
+    scanf("%d", &pessoa);
 
-void buscar (){
-
-
-}
-
-void listar_contatos (){
-
-    for (int i = 0; i < n; i++){
-        printf ("O nome eh %s, o telefone eh %s, o email eh %s, o endereco eh %s", agenda.contato [i].nome, agenda.contato [i].telefone, agenda.contato [i].email, agenda.contato [i].endereco);
+    for (int i = 0; i < *n; i++){
+        if (pessoa == i+1){
+            printf("Nome: %sTelefone: %sE-mail: %sEndereco: %s\n", agenda[i].alunos.nome, agenda[i].alunos.telefone, agenda[i].alunos.email, agenda[i].alunos.endereco);
+            printf("Deseja mesmo excluir?\n1- Sim/2- Nao");
+            scanf("%d", &resp);
+            if (resp == 1){
+               for (int i = 0) 
+            }
+            
+        }
+        else{
+            printf("Contato nao encontrado");
+        }
     }
 }
 
 int main (){
 
-    chama_menu ();
+    int n;
+
+    int opcao;
+    
+    do{
+        printf("Digite qual opcao desejada: ");
+        printf("\n1 - Inserir\n2 - Excluir\n3 - Buscar\n4 - Listar contatos\n5 - Sair\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        getchar();
+        system("cls");
+
+        switch (opcao){
+        case 1:
+            inserir(&n);
+            break;
+        case 2:
+            excluir(&n);
+            break;
+        case 3:
+            buscar(&n);
+            break;
+        case 4:
+            listar_contatos(&n);
+            break;
+        case 5:
+            break;
+        }
+    }while (opcao != 5) ;
 
     return 0;
 }
